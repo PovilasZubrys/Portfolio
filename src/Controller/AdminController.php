c7aa6295113cdbbeda1c59bdc580de9b1bf724af
Povilas Zubrys <48654760+PovilasZubrys@users.noreply.github.com>
2022-10-11 02:35:39 +0300

    Control panel WIP

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\NewProjectType;
+use App\Repository\ProjectsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +17,36 @@ class AdminController extends AbstractController
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'controller' => 'admin',
+        ]);
+    }
+
+    #[Route('/admin/new_project', name: 'new_project')]
+    public function newProject(EntityManagerInterface $em, Request $request): Response
+    {
+        $form = $this->createForm(NewProjectType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $em->persist($data);
+            $em->flush();
+
+            return $this->redirectToRoute('projects');
+        }
+        return $this->render('admin/newProject.html.twig', [
+            'controller' => 'admin',
+            'form' => $form->createView()
+        ]);
+    }
+    #[Route('/admin/projects', name: 'projects')]
+    public function projects(ProjectsRepository $projects): Response
+    {
+        $projects = $projects->findAll();
+
+        return $this->render('admin/projects.html.twig', [
+            'controller' => 'admin',
+            'projects' => $projects
         ]);
     }
 }
---
+++ src/Entity/Projects.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectsRepository::class)]
+class Projects
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $url = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image_path = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getImagePath(): ?string
+    {
+        return $this->image_path;
+    }
+
+    public function setImagePath(?string $image_path): self
+    {
+        $this->image_path = $image_path;
+
+        return $this;
+    }
+}
