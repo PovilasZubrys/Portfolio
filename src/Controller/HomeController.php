51e0862301434d9a9106d0b07843b66cba231aed
Povilas Zubrys <48654760+PovilasZubrys@users.noreply.github.com>
2022-10-11 00:19:50 +0300

    Fix.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'description' => $description,
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
