d33406689dd102104ad0b4fb39387e83d1403811
Povilas Zubrys <48654760+PovilasZubrys@users.noreply.github.com>
2022-10-17 01:10:05 +0300

    Fixing script error.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ ENDCOLOR="\e[0m"
 echo -e "${CYAN}Pulling from git${ENDCOLOR}"
 git pull
 echo "${CYAN}Updating composer${ENDCOLOR}"
-#exec ./composer.phar update
-composer update
+./composer.phar update
+#composer update
 echo "${CYAN}Pushing new migrations to database${ENDCOLOR}"
 php bin/console doctrine:migrations:migrate --no-interaction
