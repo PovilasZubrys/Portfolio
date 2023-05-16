44a395b295190cfc9220efd27b6379593aa74d9d
povilas <povilas.zubrys@pigu.lt>
2023-05-16 23:17:13 +0300

    Fix registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -14,32 +14,59 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    private $em;
+    private $userPasswordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher)
     {
+        $this->em = $entityManager;
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request): Response
+    {
+        $result = true;
+
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('home');
+        if (!empty($_POST)) {
+            $result = $this->handleData($_POST);
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
+        if (!$result || empty($_POST)) {
+            return $this->render('registration/register.html.twig');
+        }
+
+        return $this->redirectToRoute('home');
+    }
+
+    private function handleData(array $postData): bool
+    {
+        if (!isset($postData['email'])) {
+            $this->addFlash('error', 'No email set');
+            return false;
+        }
+        if (!isset($postData['password']) || !isset($postData['repeat_password'])) {
+            $this->addFlash('error', 'No password set');
+            return false;
+        }
+        if ($postData['password'] != $postData['repeat_password']) {
+            $this->addFlash('error', 'Passwords do not match');
+            return false;
+        }
+        if (strlen($postData['password']) < 8) {
+            $this->addFlash('error', 'Password too short');
+            return false;
+        }
+
+        $user = new User();
+        $user->setEmail($postData['email']);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, $postData['password']));
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return true;
     }
 }
